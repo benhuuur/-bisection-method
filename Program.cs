bddcf02a2735ac4b42faedb8b936f2eb35853739
benhur feld <benhurfeld@gmail.com>
2024-02-19 16:32:34 -0300

    false position

--- Program.cs
+++ Program.cs
@@ -3,18 +3,10 @@ using static System.Console;
 
 double myFunction(double x)
 {
-    return x + 5;
+    return x + 1;
 }
 
 var sol = Root.Bisection(myFunction, -10, +10);
 WriteLine(sol);
-using AIContinuos;
-using static System.Console;
-
-double myFunction(double x)
-{
-    return x + 5;
-}
-
-var sol = Root.Bisection(myFunction, -10, +10);
+sol = Root.FalsePosition(myFunction, -10, +10);
 WriteLine(sol);
--- ai-continuous/Root.cs
+++ ai-continuous/Root.cs
@@ -1,8 +1,48 @@
+using System;
+using static System.Console;
+
 namespace AIContinuos
 {
     public static class Root
     {
         //usar double pq tem mais precisão, pois o float tem um erro maior que double
+        public static double FalsePosition(
+            Func<double, double> function,
+            double a,
+            double b,
+            double tol = 1e-4,
+            int maxIter = 1_000
+        )
+        {
+            DateTime dateTime = DateTime.Now;
+
+            double currA = a;
+            double currB = b;
+            double fcurrA = function(a);
+            double fcurrB = function(b);
+            double c = 0;
+
+            for (int i = 0; i < maxIter; i++)
+            {
+                c = (-1 * fcurrA) * ((currB - currA) / (fcurrB - fcurrA)) + currA;
+
+                if (currB - currA < tol * 2.0)
+                    break;
+
+                var fc = function(c);
+                var fb = function(b);
+
+                if (Math.Abs(fc) < tol)
+                    break;
+
+                if (Math.Sign(fc) == Math.Sign(fb))
+                    currB = c;
+                else
+                    currA = c;
+            }
+            WriteLine($"Duration: {(DateTime.Now - dateTime).TotalMilliseconds}");
+            return c;
+        }
         public static double Bisection(
             Func<double, double> function,
             double a,
@@ -11,6 +51,8 @@ namespace AIContinuos
             int maxIter = 1_000
         )
         {
+            DateTime dateTime = DateTime.Now;
+
             double currA = a;
             double currB = b;
             double c = 0;
@@ -18,20 +60,24 @@ namespace AIContinuos
             for (int i = 0; i < maxIter; i++)
             {
                 var diff = currB - currA;
-                var half = diff / 2;
+                var half = diff / 2.0;
                 c = a + half;
 
-                if(currA - currA < tol * 2)
-                    return c;
-                
+                if(currB - currA < tol * 2.0)
+                    break;
+
                 var fc = function(c);
                 var fb = function(b);
+
+                if(Math.Abs(fc) < tol)
+                    break;
 
                 if (Math.Sign(fc) == Math.Sign(fb))
                     currB = c;
                 else
                     currA = c;
             }
+            WriteLine($"Duration: {(DateTime.Now - dateTime).TotalMilliseconds}");
             return c;
         }
     }
